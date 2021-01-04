3ac7b3742fd7e8548647bd6177ec8fd1eb9137a7
Davide <davide.zanni8@gmail.com>
2021-01-04 02:48:28 +0100

    Bullet doc

--- core/src/com/mygdx/gamepb6/bullet/Bullet.java
+++ core/src/com/mygdx/gamepb6/bullet/Bullet.java
@@ -8,6 +8,21 @@ import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.World;
 import com.mygdx.gamepb6.screens.PlayScreen;
 
+/**
+ * Bullet è una classe astratta che permette la gestione dei proiettili  durante la fase di scontro tra i giocatori.
+ * Partendo dalla classe astratta vengono successivamente implementate sottoclassi, le quali ci permettono di creare proiettili 
+ * con caratteristiche e funzionalità differenti.
+ * La classe eredita la già predefinita classe Java Sprite.
+ * La classe Sprite permette di rendere fisica l'immagine caricata come texture, contenendo informazioni relative alla geometria,colore e posizione 
+ * dell'oggetto. 
+ * 
+ * Vengono dichiarati campi che permettono di gestire e di aggiornare costantemente la posizione del proiettile come (x,y,dirx,diry) 
+ * Altri campi come screen che consentono di raffigurare il proiettile sulla mappa di gioco oppure img e atlasB contenenti la texture dell'oggetto
+ * 
+ * @author Zanni Davide 
+ * @author Golfarelli Pietro
+ *
+ */
 public abstract class Bullet extends Sprite{
 
 	public PlayScreen screen;
@@ -21,6 +36,11 @@ public abstract class Bullet extends Sprite{
 
 	public boolean vivo;
 
+	/**
+	 * Costruttore che definisce le risorse del sistema utili per la creazione dell'oggetto. 
+	 * Vengono inoltre definite le istruzioni di posizione che deve assumere il proiettile una volta definite le direzioni lungo gli assi (x,y).
+	 * 
+	 */
 	public Bullet (PlayScreen screen, float x, float y, int dirx, int diry) {
 
 		this.screen = screen;
@@ -41,14 +61,32 @@ public abstract class Bullet extends Sprite{
 
 	}
 
+
+	/**
+	 * Metodo astratto utilizzato per la distruzzione del corpo del proiettile
+	 */
 	public abstract void distruggi();
 
+	/**
+	 *  Metodo astratto utilizzato per il costante aggiornamento della posizione del proiettile e del timer relativo alla durata della vita del proiettile.
+	 *  Una volta che il timer è azzerato viene chiamato il metodo distruggi.
+	 *  
+	 *  @param bullet oggetto su cui agisce il metodo
+	 */
 	public abstract void update(float deltaTime, Bullet bullet);
 
+	/**
+	 * Metodo che richiama dalla classe padre il metodo draw per disegnare il proiettile
+	 */
 	public void draw(Batch batch) {
 		super.draw(batch);
 	}
- 
+
+	/**
+	 * Metodo astratto che definisce il corpo del proiettile, dà al corpo una propria forma fisica,gravità nella mappa e rende il corpo sensibile 
+	 * alle collisioni.
+	 * In più gestisce le direzioni e la velocità che assume il proiettile
+	 */
 	public abstract void defineBullet();
 
 }
